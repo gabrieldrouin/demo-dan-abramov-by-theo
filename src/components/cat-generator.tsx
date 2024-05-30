84b1d70817be3aef4afc2036902bb0f8f5e2f9c4
gabrieldrouin <info@gabrieldrouin.com>
2024-05-30 13:32:18 -0400

    Complete demo version a

---
+++ src/components/cat-generator.tsx
@@ -0,0 +1,15 @@
+import { readFile } from "fs/promises";
+import RevealButton from "./reveal-button";
+
+export default async function CatGenerator() {
+  const allCatsNames = await readFile("src/api/cats.txt", "utf8");
+  const cats = allCatsNames.split("\n");
+
+  const randomCat = cats[Math.floor(Math.random() * cats.length)];
+
+  return (
+    <>
+      <RevealButton catName={randomCat} />
+    </>
+  );
+}
---
+++ src/components/reveal-button.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function RevealButton(props: { catName: string }) {
+  const [revealed, setRevealed] = useState(false);
+
+  if (revealed) {
+    return (
+      <div>
+        <h1 className="text-4xl font-bold">{props.catName}</h1>
+      </div>
+    );
+  }
+
+  return (
+    <h1 className="text-4xl font-bold">
+      <button onClick={() => setRevealed(true)}>Reveal</button>
+    </h1>
+  );
+}
